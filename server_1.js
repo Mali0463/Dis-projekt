ef161acc370f8678512ab709550295023da2d53d
MalihaItsMeM <143609963+Mali0463@users.noreply.github.com>
2024-11-24 19:31:06 +0100

    done

--- server_1.js
+++ server_1.js
@@ -18,7 +18,7 @@ const db = new sqlite.Database('./Database/database.sql', (err) => {
 });
 
 // Start serveren 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
