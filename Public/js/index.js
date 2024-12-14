55c7c8171676f47d4ef2ac51f82ac6fac1f5dba7
Neschanok <nanook1210@gmail.com>
2024-12-14 23:15:57 +0100

    din mor

--- Public/js/index.js
+++ Public/js/index.js
@@ -102,7 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             try {
                 // Send data til serveren via POST-forespørgsel
-                const response = await fetch('http://localhost/3000/login', {
+                const response = await fetch('http://localhost/5000/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
